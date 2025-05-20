b045b6cde96ae4c99c9f2127f7a803db99e58478
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-05-20 08:17:35 +0530

    Created Carousel Component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Slider from "../Slider/Slider";
 
+
 const Main = () => {
   return (
     <div>
       <Navbar />
       <Slider />
+
     </div>
   );
 };
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -4,14 +4,77 @@ import {
   prevSlide,
   dotSlide,
 } from "../../features/slices/sliderSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { sliderData } from "../../assets/data/dummyData";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 const Slider = () => {
   const dispatch = useDispatch();
+  const slideIndex = useSelector((state) => state.slider.value);
+
   return (
-    <div>
-      <button>Next</button>
-      <button>Prev</button>
+    <div className="relative pb-4">
+      {/* SLIDE IMAGE SECTION */}
+      <div>
+        {sliderData.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className={
+                parseInt(item.id) === slideIndex
+                  ? "opacity-100 duration-700 ease-in-out scale-100"
+                  : "hidden"
+              }
+            >
+              <div>
+                {parseInt(item.id) === slideIndex && (
+                  <img className="h-[850px] w-full" src={item.img} />
+                )}
+              </div>
+
+              <div className="absolute top-44 mx-auto inset-x-1/4 text-center">
+                <p className="text-white text-4xl font-[inter] font-bold tracking-normal leading-none">
+                  {parseInt(item.id) === slideIndex && item.text}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      {/* DOT SLIDE */}
+      <div className="flex absolute bottom-12 left-[45%]">
+        {sliderData.map((dot, index) => {
+          return (
+            <div className="mr-4" key={index}>
+              <div
+                className={
+                  index === slideIndex
+                    ? "bg-green-600 rounded-full p-4 cursor-pointer"
+                    : "bg-white rounded-full p-4 cursor-pointer"
+                }
+                onClick={() => dispatch(dotSlide(index))}
+              ></div>
+            </div>
+          );
+        })}
+      </div>
+
+      {/* NEXT & PREVIOUS BUTTONS */}
+      <div>
+        <button
+          className="absolute top-[50%] right-4 bg-white rounded-full p-2 cursor-pointer hover:bg-green-600 hover:text-white"
+          onClick={() => dispatch(nextSlide(slideIndex + 1))}
+        >
+          <IoIosArrowForward />
+        </button>
+        <button
+          className="absolute top-[50%] left-4 bg-white rounded-full p-2 cursor-pointer hover:bg-green-600 hover:text-white"
+          onClick={() => dispatch(prevSlide(slideIndex - 1))}
+        >
+          <IoIosArrowBack />
+        </button>
+      </div>
     </div>
   );
 };
