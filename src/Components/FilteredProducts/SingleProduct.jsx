aef55f062cf4eacacbe5a651a312d6a6fbe623d1
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-05-23 12:24:52 +0530

    Completed buillding UI of Single Page Application

--- src/Components/FilteredProducts/SingleProduct.jsx
+++ src/Components/FilteredProducts/SingleProduct.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { Tooltip, Button } from "@material-tailwind/react";
 
 const SingleProduct = () => {
   const product = useSelector((state) => state.products.singleProduct);
-  console.log("singleProduct", product);
+  const productSize = product[0].size ? product[0].size[0] : "";
+  const productColor = product[0] ? product[0].color[0] : "";
+  const [size, setSize] = useState(productSize);
+  const [color, setColor] = useState(productColor);
 
   return (
     <div>
@@ -19,10 +23,70 @@ const SingleProduct = () => {
             </div>
             <div className="grow-[3]">
               <div className="max-w-lg">
-                <h5 className="text-2xl font-[inter] font-bold tracking-normal leading-none">
+                <h5 className="text-2xl font-[inter] font-bold tracking-normal leading-none pb-4">
                   {item.name}
                 </h5>
-                <p className="text-orange"></p>
+                <p className="text-orange-700 text-xl font-[inter] font-bold tracking-normal leading-none pb-4">
+                  15% OFF
+                </p>
+                <p className="text-gray-600 text-xl font-[inter] font-bold tracking-normal leading-none pb-4">
+                  {item.text}
+                </p>
+                <div className="pb-4">
+                  {product[0].size ? (
+                    <div>
+                      <label
+                        htmlFor="size"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Pick a size
+                      </label>
+                      <select
+                        id="size"
+                        name="size"
+                        value={size}
+                        onChange={(e) => setSize(e.target.value)}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      >
+                        {product[0].size.map((size, index) => {
+                          return (
+                            <option key={index} value={size}>
+                              {size}
+                            </option>
+                          );
+                        })}
+                      </select>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className="pb-4">
+                  <label
+                    htmlFor="color"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Pick a color
+                  </label>
+                  <select
+                    id="color"
+                    name="color"
+                    value={color}
+                    onChange={(e) => setColor(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  >
+                    {product[0].color.map((color, index) => {
+                      return (
+                        <option key={index} value={color}>
+                          {color}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                <Tooltip content="Add to Cart" placement="bottom" className="p-2">
+                  <button  className="border rounded-sm px-5 py-2 bg-gray-100 text-gray-600 cursor-pointer">Add to Cart</button>
+                </Tooltip>
               </div>
             </div>
           </div>
